f01eee9a6c6223556ce6457f24626497519fd94c
Nahuel Bentos <nahuelbentosgnocchi@gmail.com>
2020-08-13 08:43:28 -0300

    avance seccion modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component'; 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    
     NopagefoundComponent,
-    
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    PagesModule,
+    AuthModule
+
   ],
   bootstrap: [AppComponent]
 })
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
+
+
+
 
 
 
@@ -15,7 +21,11 @@ import { PagesComponent } from './pages.component';
     Grafica1Component,
     PagesComponent,
   ],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ],
   exports: [
     DashboardComponent,
     ProgressComponent,
